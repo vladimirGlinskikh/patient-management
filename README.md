25d62ee66a402a13e404ba604b4e4d20794e77c6
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2025-03-09 17:57:05 +0300

    Start project

---
+++ README.md
@@ -0,0 +1,1 @@
+This application is a test application for experiments with microservice architecture# patient-management
---
+++ patient-service/src/main/java/kz/zhelezyaka/patientservice/PatientServiceApplication.java
@@ -0,0 +1,13 @@
+package kz.zhelezyaka.patientservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientServiceApplication.class, args);
+	}
+
+}
