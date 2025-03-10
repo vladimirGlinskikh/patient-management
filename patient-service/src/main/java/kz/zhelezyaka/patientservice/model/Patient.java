3246570c5d8c6f35002a356baf295ec7e94711b5
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2025-03-10 17:05:42 +0300

    Create a model and docker-compose

---
+++ patient-service/src/main/java/kz/zhelezyaka/patientservice/model/Patient.java
@@ -0,0 +1,37 @@
+package kz.zhelezyaka.patientservice.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+public class Patient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    @Email
+    @Column(unique = true)
+    private String email;
+
+    @NotNull
+    private String address;
+
+    @NotNull
+    private LocalDate dateOfBirth;
+
+    @NotNull
+    private LocalDate registeredDate;
+}
