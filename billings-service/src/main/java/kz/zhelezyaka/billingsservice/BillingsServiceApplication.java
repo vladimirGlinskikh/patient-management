654b056bd2d64c2f642677a487dc530060954415
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2025-03-14 16:57:23 +0300

    Started billings-service

---
+++ billings-service/src/main/java/kz/zhelezyaka/billingsservice/BillingsServiceApplication.java
@@ -0,0 +1,13 @@
+package kz.zhelezyaka.billingsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillingsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BillingsServiceApplication.class, args);
+    }
+
+}
---
+++ billings-service/src/test/java/kz/zhelezyaka/billingsservice/BillingsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.zhelezyaka.billingsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
